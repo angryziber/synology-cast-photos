6183bcdf42bebefdc5034b2fcf2623d684aff7b9
Anton Keks <anton@codeborne.com>
2014-06-05 00:33:43 +0300

    add more meta

--- photo_meta.php
+++ photo_meta.php
@@ -1,16 +1,24 @@
 <?
-$file=$_GET['file'];
+$file = $_GET['file'];
 $prefix = '/volume1/Photos/';
 $path = $prefix.$file;
+$exiv2 = '/usr/syno/bin/exiv2';
 
 if (preg_match("/\.(CR2|cr2)\.jpg$/", $path)) {
   $path = preg_replace("/\.jpg$/", "", $path);
 }
 
+function exif_value($line) {
+  return preg_split("/\s+/", $line)[5];
+}
+
 if (file_exists($path)) {
-  $exec = exec("/usr/syno/bin/exiv2 -g Exif.Image.Orientation -pv \"".$path."\"");
-  $exif = preg_split("/\s+/", $exec);
-  $result = array('file' => $file, 'orientation' => $exif[5]);
+  exec("$exiv2 -g Exif.Image.Orientation -g Exif.Image.DateTime -g Exif.Photo.ExposureTime -g Exif.Photo.FNumber -pv ".escapeshellarg($path), $lines);
+  $result = array('file' => $file,
+                  'orientation' => exif_value($lines[0]),
+                  'datetime' => exif_value($lines[1]),
+                  'exposure' => exif_value($lines[2]),
+                  'fnumber' => exif_value($lines[3]));
 
   header("Content-type: application/json");
   header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
