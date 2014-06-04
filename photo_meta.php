8b32b237d0890bbd6c962a776c43413dfaecf9e4
Anton Keks <anton@codeborne.com>
2014-06-04 23:53:24 +0300

    php indentation

--- photo_meta.php
+++ photo_meta.php
@@ -2,19 +2,21 @@
 $file=$_GET['file'];
 $prefix = '/volume1/Photos/';
 $path = $prefix.$file;
+
 if (preg_match("/\.(CR2|cr2)\.jpg$/", $path)) {
-    $path = preg_replace("/\.jpg$/", "", $path);
+  $path = preg_replace("/\.jpg$/", "", $path);
 }
+
 if (file_exists($path)) {
-    $exec = exec("/usr/syno/bin/exiv2 -g Exif.Image.Orientation -pv \"".$path."\"");
-    $exif = preg_split("/\s+/", $exec);
-    $result = array('file' => $file, 'orientation' => $exif[5]);
-    
-    header("Content-type: application/json");
-    header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
-    echo json_encode($result);
+  $exec = exec("/usr/syno/bin/exiv2 -g Exif.Image.Orientation -pv \"".$path."\"");
+  $exif = preg_split("/\s+/", $exec);
+  $result = array('file' => $file, 'orientation' => $exif[5]);
+
+  header("Content-type: application/json");
+  header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
+  echo json_encode($result);
 }
 else {
-    echo "{}";
+  echo "{}";
 }
 ?>
--- photos_dirs.php
+++ photos_dirs.php
@@ -1,12 +1,11 @@
 <?
-    $prefix = '/volume1/Photos/';
-        
-    $dir = $_GET['dir'] ? $_GET['dir'] : '2013/Annabel';
-    header('Content-type: text/plain');
-    header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($prefix)).' GMT');
-        
-    $dir = escapeshellarg($dir);
-    chdir($prefix);
-    passthru("find $dir* -type d -maxdepth 1");
-?>
+$prefix = '/volume1/Photos/';
 
+$dir = $_GET['dir'] ? $_GET['dir'] : '2013/Annabel';
+header('Content-type: text/plain; charset=utf8');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($prefix)).' GMT');
+
+$dir = escapeshellarg($dir);
+chdir($prefix);
+passthru("find $dir* -type d -maxdepth 1");
+?>
