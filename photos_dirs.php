c2770e577bf2e9dbbcfb30db114a624fe3f3c6e4
Anton Keks <anton@codeborne.com>
2015-03-22 23:46:22 +0200

    start introducing accessToken to some urls

--- photos_dirs.php
+++ photos_dirs.php
@@ -1,6 +1,13 @@
 <?
 $prefix = '/volume1/Photos';
 
+$accessToken = trim(file($prefix.'/php-access-token.txt')[0]);
+if ($_GET['accessToken'] != $accessToken) {
+    header("HTTP/1.0 403 Forbidden");
+    echo "Forbidden";
+    return;
+}
+
 $dir = $_GET['dir'];
 header('Content-type: text/plain; charset=utf8');
 header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($prefix)).' GMT');
