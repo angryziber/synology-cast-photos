af1af98f7acdb07a7d7648c95d1ca37f2b27adc9
Anton Keks <anton@codeborne.com>
2015-04-01 00:42:48 +0300

    reduce metadata postprocessing on the server side

--- backend/photo_meta.php
+++ backend/photo_meta.php
@@ -17,14 +17,14 @@ header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
 
 exec("$exiv2 -g Exif.Image.Model -g Exif.Image.Orientation -g Exif.Image.DateTime -g Exif.Photo.ExposureTime -g Exif.Photo.FNumber -g Exif.Photo.ISOSpeedRatings -g Exif.Photo.FocalLength -g Exif.CanonCs.LensType -PEIkvt ".escapeshellarg($path), $lines, $return);
 $result = array('file' => $file,
-              'camera' => str_replace(' EOS', '', str_replace('DIGITAL REBEL', '300D', str_replace('DIGITAL REBEL XT', '350D', exif_value($lines[0], 2)))),
+              'camera' => str_replace('DIGITAL REBEL', '300D', str_replace('DIGITAL REBEL XT', '350D', exif_value($lines[0], 2))),
               'orientation' => exif_value($lines[1], 1),
               'date' => exif_value(preg_replace('/:/', '-', $lines[2], 2), 1),
               'exposure' => exif_value($lines[3], 1),
               'fnumber' => exif_value($lines[4], 2),
               'iso' => exif_value($lines[5], 1),
               'focal' => exif_value($lines[6], 2),
-              'lens' => preg_replace('/Canon EF(-S)?/', '', str_replace('(65535)', '', exif_value($lines[7], 2))));
+              'lens' => str_replace('(65535)', '', exif_value($lines[7], 2)));
 
 echo json_encode($result);
 ?>
