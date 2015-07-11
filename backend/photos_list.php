e911e95e13873afc59afc76bd0d384f5fc23a182
Anton Keks <anton@codeborne.com>
2015-07-11 15:37:29 +0300

    allow specifying a list of several dirs at once separated with + sign

--- backend/photos_list.php
+++ backend/photos_list.php
@@ -3,13 +3,17 @@ include 'config.php';
 
 $dir = $_GET['dir'];
 chdir($photos_list_dir);
-ensure_safe($dir);
+
+$dirs = explode('+', $dir);
+
+foreach ($dirs as $dir) {
+  ensure_safe($dir);
+  $dir = escapeshellarg($dir);
+  exec("find $dir* -type f", $files);
+}
 
 header('Content-type: text/plain; charset=utf8');
 header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime("$photos_list_dir/$dir")).' GMT');
-
-$dir = escapeshellarg($dir);
-exec("find $dir* -type f", $files);
 
 foreach ($files as $file) {
   $ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
