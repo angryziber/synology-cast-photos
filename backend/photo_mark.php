bafae3f53c89b0385e569f86cfdf5bd619c6f214
Anton Keks <anton@codeborne.com>
2015-07-17 00:00:33 +0300

    support for unmarking

--- backend/photo_mark.php
+++ backend/photo_mark.php
@@ -9,8 +9,18 @@ $how = $_POST['how'];
 header("Content-type: text/plain");
 
 $what = "$file $how";
+$what_escaped = str_replace(" ", "\\ ", $what);
+exec("fgrep $what_escaped $marks_file", $matches);
+
+$removals = count(preg_grep('/^-/', $matches));
+$marked = count($matches) - $removals > $removals;
+
+if ($marked) {
+    $what = '-'.$what;
+}
+
 if (file_put_contents($marks_file, $what."\n", FILE_APPEND)) {
-    echo "Marked $what";
+    echo $marked ? "Unmarked $how" : "Marked $how";
 }
 else {
     echo "Failed to mark $what";
