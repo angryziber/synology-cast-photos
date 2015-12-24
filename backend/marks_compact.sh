1da4deb776ef4313d28204ca0e3376db04e3b967
Anton Keks <anton@azib.net>
2015-12-25 01:03:01 +0200

    scripts for really deleting of marked files

---
+++ backend/marks_compact.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+# This script removes umarked lines from marks.txt (starting with -)
+
+declare -A lines
+
+IFS=$'\n'
+for line in `cat marks.txt`; do
+  if [ ${line:0:1} == '-' ]; then
+    lines[${line:1}]=0
+  else
+    lines[$line]=1
+  fi
+done
+
+mv marks.txt marks.txt~
+
+for line in "${!lines[@]}"
+do
+  if [ ${lines[$line]} == 1 ]; then
+    echo $line >> marks.txt
+  fi
+done
+
+wc -l marks.txt marks.txt~
+
---
+++ backend/marks_delete.sh
@@ -0,0 +1,18 @@
+#!/bin/bash -e
+# This script deletes files marked as so in marks.txt by photo_mark.php
+
+./marks_compact.sh
+
+IFS=$'\n'
+for line in `grep ' delete$' marks.txt`; do
+  file=${line% delete}
+  real_file=${file/%.CR2.jpg/.CR2}
+  real_file=${real_file/%.cr2.jpg/.cr2}
+
+  if [ -e $real_file ]; then
+    eog "$real_file"
+  else
+    echo "$real_file already deleted, removing"
+    sed -i "/${real_file//\//\\/}/d" marks.txt
+  fi
+done
