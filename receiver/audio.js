5ab22303aa89fa66766375494c16c9c571111bb3
Anton Keks <anton@azib.net>
2016-01-03 19:52:49 +0200

    fix displaying of title

--- receiver/audio.js
+++ receiver/audio.js
@@ -1,17 +1,19 @@
 var audio = (function(self) {
   var player = $('audio')[0];
+  var $title = $('#title');
   var playlist = [];
   var playlistIndex = 0;
 
   self.addToPlaylist = function(url, name) {
     playlist.push({url:url, name:name});
-    if (!player.src) self.play();
+    if (!player.src || player.paused) self.play();
+    else $title.text('Added (' + playlist.length + '): ' + name);
   };
 
   self.play = function() {
     player.src = playlist[playlistIndex].url;
     player.play();
-    $title.text('Playing: ' + playlist[playlistIndex].name);
+    $title.text('Playing (' + (playlistIndex+1) + '/' + playlist.length + '): ' + playlist[playlistIndex].name);
   };
 
   self.stop = function() {
