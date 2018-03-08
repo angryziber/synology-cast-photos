6795b60db6f0b7753a6d04e8c8d337c6c8b0dfdb
Anton Keks <anton@azib.net>
2018-03-08 20:00:30 +0200

    display index in the status field

--- receiver/videos.js
+++ receiver/videos.js
@@ -3,17 +3,20 @@ function Videos(config) {
   var self = Object.assign(this, config);
 
   var title = document.getElementById('title');
+  var status = document.getElementById('status');
   var video = document.getElementsByTagName('video')[0];
 
   video.addEventListener('ended', function() {
     self.next();
   });
   video.addEventListener('canplaythrough', function () {
+    status.textContent = self.index + '/' + self.urls.length;
     video.play();
   });
 
   self.loadCurrent = function() {
     var url = self.currentUrl();
+    status.textContent = 'Loading ' + self.index + '/' + self.urls.length;
     video.setAttribute('src', config.videoUrlPrefix + url);
     video.pause();
     self.title(url.substring(0, url.lastIndexOf('/')).replace('/', ' / '));
