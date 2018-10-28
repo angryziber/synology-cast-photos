e9621addf062f1855039161e6d6095c4b2019e09
Anton Keks <anton@azib.net>
2018-10-28 22:04:27 +0200

    add videos dir to backend

---
+++ backend/videos_dirs.php
@@ -0,0 +1,15 @@
+<?
+include 'config.php';
+check_access();
+
+$dir = $_GET['dir'];
+chdir($videos_dir);
+ensure_safe(dirname($dir));
+
+header('Content-type: text/plain; charset=utf8');
+header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($photos_list_dir)).' GMT');
+
+$dir = escapeshellarg($dir);
+passthru("find $dir* -type d -maxdepth 1", $status);
+if ($status != 0) passthru("find -iname '*$dir*' -type d -maxdepth 2 -not -path '**/@eaDir*' | sed 's@./@@'");
+?>
