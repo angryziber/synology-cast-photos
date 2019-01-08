87a72df54d2d155610c80ade01cd79f1cb8fed5c
Anton Keks <anton@azib.net>
2019-01-09 00:43:49 +0200

    specify content-length

--- backend/photov.php
+++ backend/photov.php
@@ -8,9 +8,13 @@ header("Content-type: video/mp4");
 $outfile = tempnam(sys_get_temp_dir(), 'img2mp4').".mp4";
 header("Content-disposition: inline; filename=" . basename($outfile));
 header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
+
+exec("/bin/ffmpeg -hide_banner -i '$path' -pix_fmt yuvj420p -tune stillimage -preset superfast -vf scale=w=3840:h=2160:force_original_aspect_ratio=decrease -r 1 '$outfile'");
+header('Content-Length: ' . filesize($outfile));
+header('Accept-Ranges: none');
+
 ob_clean();
 flush();
-exec("/bin/ffmpeg -hide_banner -i '$path' -pix_fmt yuvj420p -tune stillimage -preset superfast -vf scale=w=3840:h=2160:force_original_aspect_ratio=decrease -r 1 '$outfile'");
 readfile($outfile);
 unlink($outfile);
 ?>
