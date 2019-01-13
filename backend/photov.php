44acdca7bc46d15a391b676b936653cba4ef3dcd
Anton Keks <anton@azib.net>
2019-01-13 23:37:06 +0200

    extract w & h variables

--- backend/photov.php
+++ backend/photov.php
@@ -9,7 +9,10 @@ $outfile = tempnam(sys_get_temp_dir(), 'img2mp4').".mp4";
 header("Content-disposition: inline; filename=" . basename($outfile));
 header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
 
-exec("/bin/ffmpeg -hide_banner -i '$path' -pix_fmt yuvj420p -tune stillimage -preset superfast -vf scale=w=3840:h=2160:force_original_aspect_ratio=decrease,pad=3840:2160:'(ow-iw)/2':'(oh-ih)/2' -r 1 '$outfile'");
+$w = 3840;
+$h = 2160;
+
+exec("/bin/ffmpeg -hide_banner -i '$path' -pix_fmt yuv420p -profile high -tune stillimage -preset superfast -vf scale=w=$w:h=$h:force_original_aspect_ratio=decrease,pad=$w:$h:'(ow-iw)/2':'(oh-ih)/2' -r 1 '$outfile'");
 header('Content-Length: ' . filesize($outfile));
 header('Accept-Ranges: none');
 
