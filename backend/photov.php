cdb9619074d4d6c63d14123b4666107d79a806fa
Anton Keks <anton@azib.net>
2019-01-13 22:21:17 +0200

    fit converted video into black bars to avoid scaling on the client

--- backend/photov.php
+++ backend/photov.php
@@ -9,7 +9,7 @@ $outfile = tempnam(sys_get_temp_dir(), 'img2mp4').".mp4";
 header("Content-disposition: inline; filename=" . basename($outfile));
 header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
 
-exec("/bin/ffmpeg -hide_banner -i '$path' -pix_fmt yuvj420p -tune stillimage -preset superfast -vf scale=w=3840:h=2160:force_original_aspect_ratio=decrease -r 1 '$outfile'");
+exec("/bin/ffmpeg -hide_banner -i '$path' -pix_fmt yuvj420p -tune stillimage -preset superfast -vf scale=w=3840:h=2160:force_original_aspect_ratio=decrease,pad=3840:2160:'(ow-iw)/2' -r 1 '$outfile'");
 header('Content-Length: ' . filesize($outfile));
 header('Accept-Ranges: none');
 
