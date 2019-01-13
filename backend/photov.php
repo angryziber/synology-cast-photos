1ece70d982acdd82a3a1d0ac0b1fa5a44626703d
Anton Keks <anton@azib.net>
2019-01-13 23:40:33 +0200

    extract scale option to a variable

--- backend/photov.php
+++ backend/photov.php
@@ -11,8 +11,10 @@ header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
 
 $w = 3840;
 $h = 2160;
+$scale = "-vf scale=w=$w:h=$h:force_original_aspect_ratio=decrease,pad=$w:$h:'(ow-iw)/2':'(oh-ih)/2'";
+# TODO: https://stackoverflow.com/questions/30789367/ffmpeg-how-to-convert-vertical-video-with-black-sides-to-video-169-with-blur
 
-exec("/bin/ffmpeg -hide_banner -i '$path' -pix_fmt yuv420p -profile high -tune stillimage -preset superfast -vf scale=w=$w:h=$h:force_original_aspect_ratio=decrease,pad=$w:$h:'(ow-iw)/2':'(oh-ih)/2' -r 1 '$outfile'");
+exec("/bin/ffmpeg -hide_banner -i '$path' -pix_fmt yuv420p -profile high -tune stillimage -preset superfast $scale -r 1 '$outfile'");
 header('Content-Length: ' . filesize($outfile));
 header('Accept-Ranges: none');
 
