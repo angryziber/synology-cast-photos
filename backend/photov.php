e075fff7071391f04472eb1ed2847e7a1916202e
Anton Keks <anton@azib.net>
2019-01-13 23:47:24 +0200

    pad with blurred image instead of black bars - better for Ambilight TVs
    https://stackoverflow.com/questions/43577377/extend-scale-a-portrait-video-so-the-aspect-ratio-stays-and-the-black-borders

--- backend/photov.php
+++ backend/photov.php
@@ -11,8 +11,8 @@ header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
 
 $w = 3840;
 $h = 2160;
-$scale = "-vf scale=w=$w:h=$h:force_original_aspect_ratio=decrease,pad=$w:$h:'(ow-iw)/2':'(oh-ih)/2'";
-# TODO: https://stackoverflow.com/questions/30789367/ffmpeg-how-to-convert-vertical-video-with-black-sides-to-video-169-with-blur
+#$scale = "-vf scale=w=$w:h=$h:force_original_aspect_ratio=decrease,pad=$w:$h:'(ow-iw)/2':'(oh-ih)/2'";
+$scale = "-filter_complex '[0]scale=$w:$h,setsar=1,boxblur=20:20[b];[0]scale=-1:${h}[v];[b][v]overlay=(W-w)/2'";
 
 exec("/bin/ffmpeg -hide_banner -i '$path' -pix_fmt yuv420p -profile high -tune stillimage -preset superfast $scale -r 1 '$outfile'");
 header('Content-Length: ' . filesize($outfile));
