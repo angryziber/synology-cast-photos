e5fc21d35e2e814584f294819d9bf07815a5c64c
Anton Keks <anton@azib.net>
2019-01-14 00:56:41 +0200

    read exif and rotate (transpose) input if needed - ffmpeg still cannot do it itself

--- backend/photov.php
+++ backend/photov.php
@@ -11,11 +11,18 @@ header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
 
 $w = 3840;
 $h = 2160;
-#$scale = "-vf scale=w=$w:h=$h:force_original_aspect_ratio=decrease,pad=$w:$h:'(ow-iw)/2':'(oh-ih)/2'";
-$scale = "-filter_complex '[0]scale=$w:$h,setsar=1,boxblur=20:20[b];[0]scale=-1:${h}[v];[b][v]overlay=(W-w)/2'";
-$codec = "-vcodec libx264 -profile high -tune stillimage -preset superfast";
 
-exec("/bin/ffmpeg -hide_banner -i '$path' $codec -pix_fmt yuv420p $scale -r 1 '$outfile'");
+$exif_orientation = exec("exiv2 -g Exif.Image.Orientation -Pv $path");
+$transpose = "";
+if ($exif_orientation == 6) $transpose = "transpose=1,";
+else if ($exif_orientation == 8) $transpose = "transpose=2,";
+else if ($exif_orientation == 3) $transpose = "transpose=1,transpose=1,";
+
+#$scale = "-vf scale=${transpose},w=$w:h=$h:force_original_aspect_ratio=decrease,pad=$w:$h:'(ow-iw)/2':'(oh-ih)/2'";
+$scale = "-filter_complex '[0]${transpose}scale=$w:$h,setsar=1,boxblur=20:20[b];[0]${transpose}scale=-1:${h}[v];[b][v]overlay=(W-w)/2'";
+$codec = "-vcodec libx264 -profile:v high -tune stillimage -preset superfast";
+
+exec("/bin/ffmpeg -hide_banner -noautorotate -i '$path' $codec -pix_fmt yuv420p $scale -r 1 '$outfile'");
 header('Content-Length: ' . filesize($outfile));
 header('Accept-Ranges: none');
 
