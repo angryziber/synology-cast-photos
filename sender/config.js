53a83f65100aeb3794c01b9881ff4270686a2fdb
Anton Keks <anton@azib.net>
2019-01-30 00:30:16 +0200

    vk support dropped some time ago

--- sender/config.js
+++ sender/config.js
@@ -3,11 +3,6 @@ var chromecast = {
   appId: location.host.indexOf('192.168.') >= 0 || location.host.indexOf('.local') >= 0 ? '40FA4E04' : '87673D37'
 };
 
-var vk = {
-  // VK client id in order to get access to audio search there
-  clientId: 4774342
-};
-
 var sender = {
   photoDirsSuggestUrl: '/backend/photos_dirs.php'
 };
