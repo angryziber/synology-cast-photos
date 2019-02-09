5d8655de989bfe144321f3625c0221e3031c08fd
Anton Keks <anton@azib.net>
2019-02-09 14:30:42 +0200

    increase depth of dir searching

--- backend/videos_dirs.php
+++ backend/videos_dirs.php
@@ -10,6 +10,6 @@ header('Content-type: text/plain; charset=utf8');
 header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($photos_list_dir)).' GMT');
 
 $dir = escapeshellarg($dir);
-passthru("find $dir* -type d -maxdepth 1", $status);
-if ($status != 0) passthru("find -iname '*$dir*' -type d -maxdepth 2 -not -path '**/@eaDir*' | sed 's@./@@'");
+passthru("find $dir* -type d -maxdepth 1 -not -path '**/@eaDir*'", $status);
+if ($status != 0) passthru("find -iname '*$dir*' -type d -maxdepth 3 -not -path '**/@eaDir*' | sed 's@./@@'");
 ?>
