563c5c566db345363a2acafd2dba9128f149d5b8
Anton Keks <anton@azib.net>
2020-11-01 22:54:35 +0200

    ini php || operator returns a boolean, not the second argument

--- backend/photov.php
+++ backend/photov.php
@@ -10,10 +10,17 @@ $preload = $_GET['preload'];
 $outfile = sys_get_temp_dir().'/img2mp4$'.str_replace('/', '$', $file).'.mp4';
 $exists = file_exists($outfile);
 
-if (!$exists) {
-  $w = $_GET['w'] || 3840;
-  $h = $_GET['h'] || 2160;
+$w = $_GET['w'];
+if (!$w) $w = 3180;
+$h = $_GET['h'];
+if (!$h) $h = 2160;
 
+
+# TODO VideoStation with HW acceleration (may be problematic with parallel processing)
+# /var/packages/VideoStation/target/bin/ffmpeg -vaapi_device /dev/dri/renderD128 -hwaccel vaapi -hwaccel_output_format vaapi -noautorotate -i /volume1/downloads/Popeye.1980.1080p.WEB-DL.H264-iDLE [PublicHD]/Popeye.1980.1080p.WEB-DL.DD5.1.h264-iDLE.mkv -vcodec h264_vaapi -vf format=nv12|vaapi,hwupload,scale_vaapi=w=848:h=352 -vsync 2 -bf 0 -b:v 3192698 -acodec aac -ab 128k -ac 2 -progress - -f mp4 -skip_displaymatrix 1 -map 0:0 -map 0:1 -y /volume1/downloads/@eaDir/@tmp/3f7fab36691924c981a926eb85ffad1a_64007726
+# Parent: /var/packages/VideoStation/target/bin/synovideoconversion -i /volume1/downloads/Popeye.1980.1080p.WEB-DL.H264-iDLE [PublicHD]/Popeye.1980.1080p.WEB-DL.DD5.1.h264-iDLE.mkv -o /volume1/downloads/Popeye.1980.1080p.WEB-DL.H264-iDLE [PublicHD]/Popeye.1980.1080p.WEB-DL.DD5.1.h264-iDLE (medium).mp4 -v medium -a -1 -u 1026
+
+if (!$exists) {
   $exif_orientation = exec("exiv2 -g Exif.Image.Orientation -Pv '$path'");
   $transpose = "";
   if ($exif_orientation == 6) $transpose = "transpose=1,";
@@ -33,7 +40,7 @@ if (!$exists) {
 header('Last-Modified: '.gmdate('D, d M Y H:i:s', filemtime($path)).' GMT');
 header('Accept-Ranges: none');
 
-if ($preload) {
+if ($preload) {;
   echo filesize($outfile);
 }
 else {
