1f3c0f99a61954f36f77532476d5c2bddc9ccbeb
Anton Keks <anton@codeborne.com>
2021-01-10 20:05:22 +0200

    fix variable name

--- receiver/content.js
+++ receiver/content.js
@@ -1,7 +1,7 @@
 function BaseContent(self) {
   self.urls = []
   self.index = 0
-  self.status = document.getElementById('meta')
+  self.meta = document.getElementById('meta')
   self.status = document.getElementById('status')
   self.supports4k = undefined
   self.baseUrl = ''
