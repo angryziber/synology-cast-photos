b8cb2ad9e579a742ec6b224b17a97f6b1f1603cf
Anton Keks <anton@codeborne.com>
2021-02-10 01:00:08 +0200

    update hwaccel todo

--- backend/photov.php
+++ backend/photov.php
@@ -16,9 +16,8 @@ $h = $_GET['h'];
 if (!$h) $h = 2160;
 
 
-# TODO VideoStation with HW acceleration (may be problematic with parallel processing)
-# /var/packages/VideoStation/target/bin/ffmpeg -vaapi_device /dev/dri/renderD128 -hwaccel vaapi -hwaccel_output_format vaapi -noautorotate -i /volume1/downloads/Popeye.1980.1080p.WEB-DL.H264-iDLE [PublicHD]/Popeye.1980.1080p.WEB-DL.DD5.1.h264-iDLE.mkv -vcodec h264_vaapi -vf format=nv12|vaapi,hwupload,scale_vaapi=w=848:h=352 -vsync 2 -bf 0 -b:v 3192698 -acodec aac -ab 128k -ac 2 -progress - -f mp4 -skip_displaymatrix 1 -map 0:0 -map 0:1 -y /volume1/downloads/@eaDir/@tmp/3f7fab36691924c981a926eb85ffad1a_64007726
-# Parent: /var/packages/VideoStation/target/bin/synovideoconversion -i /volume1/downloads/Popeye.1980.1080p.WEB-DL.H264-iDLE [PublicHD]/Popeye.1980.1080p.WEB-DL.DD5.1.h264-iDLE.mkv -o /volume1/downloads/Popeye.1980.1080p.WEB-DL.H264-iDLE [PublicHD]/Popeye.1980.1080p.WEB-DL.DD5.1.h264-iDLE (medium).mp4 -v medium -a -1 -u 1026
+# TODO VideoStation with HW acceleration can use something like that:
+# /var/packages/VideoStation/target/bin/ffmpeg -vaapi_device /dev/dri/renderD128 -hwaccel vaapi -hwaccel_output_format vaapi -noautorotate -i input-file -vcodec h264_vaapi -vf format=nv12|vaapi,hwupload,scale_vaapi=w=$w:h=$h -vsync 2 -bf 0 -b:v 3192698 - -f mp4 -skip_displaymatrix 1 -map 0:0 -map 0:1 -y ???
 
 if (!$exists) {
   $exif_orientation = exec("exiv2 -g Exif.Image.Orientation -Pv '$path'");
